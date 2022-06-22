35bb45aa23fdf442e54a2f4b1ae9b3d5be64ed9a
Muhammad Arrezza Faiz Pratama <103143006+panda560@users.noreply.github.com>
2022-06-22 17:53:44 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,12 +1,12 @@
 let handler = async (m, { conn, usedPrefix }) => conn.sendButton(m.chat, `
-╭─「 Donasi • Dana 」
-│ • Tri [0895336282144]
-│ • Gopay  [0895336282144]
-│ • Dana  [0895336282144]
-│ • Saweria  [https://saweria.co/thesadboy01]
+╭─「 Donasi • Gopay 」
+│ • Xl [+62 819-5929-3465]
+│ • Gopay  [+6281273677810]
+│ • Dana  []
+│ • Saweria  []
 ╰────
 ╭─「 *NOTE* 」
-│ > Ingin donasi? Wa.me/62895336282144
+│ > Ingin donasi? Wa.me/6281273677810
 │ _Hasil donasi akan digunakan buat sewa_
 │ _atau beli *RDP/VPS* agar bot bisa jalan_
 │ _24jam tanpa kendala_
